fcc9bf2f865a47d8fcec8e94b60abddefbc6baae
Maleswena <seopyanemaleswena@gmail.com>
2024-04-16 22:17:27 +0200

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,13 +12,15 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>
                options.UseSqlServer(builder.Configuration.GetConnectionString("DatingAppString")));
 
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
